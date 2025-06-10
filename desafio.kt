bc49e81935bbff51a7b66bff0fac94bf0c9ad483
Jefferson Braz <106778836+jeffbveiga@users.noreply.github.com>
2025-06-09 21:30:14 -0300

    Adicionado usuarios , conteudos e formação

--- desafio.kt
+++ desafio.kt
@@ -9,11 +9,27 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
+        
+        inscritos += usuario
+        
         //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
+    
+    var user1 = Usuario("Jefferson" , 33)
+    var user2 = Usuario("Guilherme" , 27)
+    var user3 = Usuario("Fernanda" , 19)
+    
+    val html = ConteudoEducacional("html" , 2)
+    val css = ConteudoEducacional("css" , 3)
+    
+    val frontEnd = Formacao("Front-end" , listOf (html,css))
+    
+    println(frontEnd)
+    
+   
     
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
